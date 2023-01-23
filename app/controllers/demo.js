dcc72c6263d48e5e68dea973a9afe52492228d6f
Chris Manson <chris@manson.ie>
2023-01-23 11:15:41 +0000

    add a way to toggle examples

---
+++ app/controllers/demo.js
@@ -0,0 +1,39 @@
+import Controller from '@ember/controller';
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
+
+export default class DemoController extends Controller {
+  availableVariations = [
+    'ember-css-modules',
+    'normal',
+    'simple-append',
+    'svelt',
+    'vue',
+  ];
+
+  queryParams = ['variations'];
+
+  @tracked variations = this.availableVariations;
+
+  @action
+  isChecked(variation) {
+    return this.variations.includes(variation);
+  }
+
+  @action
+  changed(variation) {
+    let newVariations;
+
+    if (this.variations.includes(variation)) {
+      newVariations = this.variations.filter((item) => item !== variation);
+    } else {
+      newVariations = [...this.variations, variation];
+    }
+
+    this.variations = newVariations.sort(
+      (a, b) =>
+        this.availableVariations.indexOf(a) -
+        this.availableVariations.indexOf(b)
+    );
+  }
+}
